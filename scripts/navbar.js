89b3ad26503c87e65281c6a46becbdddb848a2b2
Dhruv Sharma <dhruvsharma0503@gmail.com>
2019-08-30 21:15:59 +0400

    added smooth scroll + experience tab wrk started

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -2,16 +2,26 @@ const menu = document.getElementById('menu');
 const nav = document.getElementById('nav');
 const whitespace = document.getElementById('whitespace');
 const exit = document.getElementById('exit');
+
+const { SmoothScroll } = window;
+// eslint-disable-next-line no-unused-vars
+const easeInOutQuad = new SmoothScroll('[data-easing="easeInOutQuad"]', {
+  easing: 'easeInOutQuad',
+  header: '.dummy-header',
+});
+
 menu.addEventListener('click', (e) => {
   nav.classList.toggle('hide-mobile');
   whitespace.classList.toggle('hide-mobile');
   e.preventDefault();
 });
+
 exit.addEventListener('click', (e) => {
   nav.classList.toggle('hide-mobile');
   whitespace.classList.toggle('hide-mobile');
   e.preventDefault();
 });
+
 whitespace.addEventListener('click', (e) => {
   nav.classList.toggle('hide-mobile');
   whitespace.classList.toggle('hide-mobile');
